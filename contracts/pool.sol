cbe6000ea21a0edc1b96f99457ad23b86e8f0fb5
ARNAB RAY <59610398+Arnab11917676@users.noreply.github.com>
2021-08-31 11:16:19 +0530

    Update pool.sol

--- contracts/pool.sol
+++ contracts/pool.sol
@@ -1,71 +1,53 @@
 //Write a smart contract to lock Uniswap V3 NFT and issue ERC20 equal to total liquidity in that NFT w.r.t token0.
 //Eg. if token0 is DAI and token1 is ETH, and the NFT is holding 1 ETH and 1000 DAI, mint 2000 ERC20 tokens.
 
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.5;
+pragma  abicoder v2;
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
-//import "..\..\faucet\faucet-contracts\contracts\TestToken.sol";
-import "./Interfaces/IERC20.sol";
+import "./Interfaces/INonfungiblePositionMamager.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./library/tokenPosition.sol";
 
 
-contract liquidityLock {
-        using SafeMath for uint;
-        bool public done;
-        event LockedToken(uint totalStableCoin, address userAddress);
-        mapping (address => uint) public tokenAddressToValue;
-        mapping (address => uint) public tokensLocked;
-        mapping (address => uint []) public tokenDetails;
-        address public owner;
-        address public lockedAt;
-        constructor() {
-               owner = msg.sender;
+contract pool {
+    using tokenPosition for tokenPosition.tokenInfo;
+    mapping (uint => tokenPosition.tokenInfo) public lpt;
+    uint128 private constant MAX_UINT128 = type(uint128).max;
+    INonfungiblePositionManager private _uniswapNFTPosition;
+    constructor (){
+        _uniswapNFTPosition= INonfungiblePositionManager(0xDAa2031432cD9e07316A66d5c95B5A4434Ffc781);
+    }
+    function lockNft(tokenPosition.tokenInfo calldata params) external returns (bool){
+        _uniswapNFTPosition.transferFrom(msg.sender,address (this),params.tokenId);
+        params.isPositionValid();
+        params.isTokenValid(params.tokenId);
+        lpt[params.tokenId] = params;
+        return true;
+    }
+    function claimNFTFee(uint tokenId)external returns (uint amount1,uint amount2){
+        tokenPosition.tokenInfo memory params = lpt[tokenId];
+        params.isTokenValid(tokenId);
+        params.isFeeClaimAllowed();
+        (amount1,amount2) = _uniswapNFTPosition.collect(
+        INonfungiblePositionManager.CollectParams(tokenId,params.feeReceiver,MAX_UINT128,MAX_UINT128)
+        );
+        mintToken(amount1,amount2,tokenId);
+    }
+    function mintToken(uint amount1, uint amount2, uint tokenID) internal{
+        tokenPosition.tokenInfo memory params = lpt[tokenID];
+        uint amountOfTokenToMint;
+        uint temp;
+        if (amount1> amount2){
+                amountOfTokenToMint = amount2 / amount1;
+                temp = amount2 % amount1;
+                amountOfTokenToMint *= temp;
+        }else{
+            amountOfTokenToMint = amount1 / amount2;
+            temp = amount1 % amount2;
+            amountOfTokenToMint *= temp;
         }
-
-        modifier onlyOwner(){
-                require(msg.sender == owner ,'Not authorized');
-                _;
-        }
-
-        function lockNFTandMint (uint token0,uint token1, address token0Address, address token1Address,  address myTokenAddress) external onlyOwner returns(address finder){
-                uint product;
-                uint takenValue;
-                if (token0 > token1){
-                       SafeMath.div(token0,token1)*100;
-                         product.mul(2);
-                         takenValue = convertValue(token0,token1);
-                }else {
-                         product = SafeMath.div(token1,token0)*100;
-                         product.mul(2);
-                         takenValue = convertValue(token1,token0);
-                }
-
-               finder =   transferToken (lockedAt,takenValue);
-
-                tokensLocked[msg.sender] = tokensLocked[msg.sender].add(takenValue);
-                tokenDetails[msg.sender].push(token0);
-                tokenDetails[msg.sender].push(token1);
-                emit LockedToken(product,msg.sender);
-                minter(myTokenAddress,product);
-                return lockedAt;
-
-        }
-
-        function convertValue(uint tkn1, uint tkn2) internal returns (uint){
-                uint convertedValue ;
-                convertedValue  = SafeMath.div(tkn1,tkn2);
-                convertedValue.add(tkn1);
-                return convertedValue;
-        }
-
-        function minter(address _tokenToBeMinted, uint _toBeMinted )internal returns (bool){
-                IERC20(_tokenToBeMinted).mint(owner,_toBeMinted);
-                done = true;
-                return done;
-        }
-
-        function transferToken(address lockedAt, uint value) internal  returns (address){
-                        IERC20(owner).transfer(lockedAt,value);
-                        return lockedAt;
-        }
+        ERC20(params.owner,amountOfTokenToMint);
+    }
 }
